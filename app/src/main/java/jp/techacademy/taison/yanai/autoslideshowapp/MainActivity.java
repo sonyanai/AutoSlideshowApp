caa73d2a569ff635d7bb36ae1d8206e072a062cb
sonyanai <taison0930@outlook.jp>
2017-07-30 19:17:20 +0900

    ContentProvider setButtons buttonEffect autoSlides stop/start 再生中のback,forwardの無効化

--- app/src/main/java/jp/techacademy/taison/yanai/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/taison/yanai/autoslideshowapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     Button mBackButton;
     Button mStopButton;
     Button mForwardButton;
+    Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,35 +46,21 @@ public class MainActivity extends AppCompatActivity {
             // パーミッションの許可状態を確認する
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 // 許可されている
-                //getContentsInfo();
                 firstContentsInfo();
+                //firstContentsInfo();
             } else {
                 // 許可されていないので許可ダイアログを表示する
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
             }
             // Android 5系以下の場合
         } else {
-            //getContentsInfo();
             firstContentsInfo();
         }
-
-        ContentResolver resolver = getContentResolver();
-        final Cursor cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目(null = 全項目)
-                null, // フィルタ条件(null = フィルタなし)
-                null, // フィルタ用パラメータ
-                null // ソート (null ソートなし)
-        );
 
 
         mBackButton = (Button) findViewById(R.id.back_button);
         mStopButton = (Button) findViewById(R.id.startStop_button);
         mForwardButton = (Button) findViewById(R.id.forward_button);
-
-
-
-
 
 
         mBackButton.setOnClickListener(new View.OnClickListener(){//押したときの処理
@@ -90,23 +78,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 if(mTimer == null){
-                    mStopButton.setText("start");
+                    mStopButton.setText("stop");
+                    mForwardButton.setEnabled(false);//trueで表示,falseで非表示
+                    mBackButton.setEnabled(false);
                     mTimer = new Timer();//タイマーの作成
                     mTimer.schedule(new TimerTask(){
                         @Override
                         public void run(){
                             mTimerSec +=1;
                             //ここで次の写真に移動させる
-                            nextContentsInfo();
+                            mHandler.post(new Runnable(){
+                                @Override
+                                public void run(){
+                                    nextContentsInfo();
+                                }
+                            });
                         }
                     },2000,2000);
                 }else if(mTimer != null) {//動いてたら止める//再生中はstop
-                    mStopButton.setText("stop");
+                    mStopButton.setText("start");
+                    mForwardButton.setEnabled(true);
+                    mBackButton.setEnabled(true);
                     mTimer.cancel();
                     mTimer = null;
                 }
             }
         });
+
+
 
         mForwardButton.setOnClickListener(new View.OnClickListener(){//押したときの処理
             @Override
@@ -125,8 +124,15 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case PERMISSIONS_REQUEST_CODE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //getContentsInfo();
                     firstContentsInfo();
+
+
+
+
+
+
+
+
                 }
                 break;
             default:
@@ -134,37 +140,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    ContentResolver resolver = getContentResolver();
-    Cursor cursor = resolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            null,
-            null,
-            null,
-            null
-    );
 
-    //情報を取得
-    /*private void getContentsInfo() {
+    ContentResolver resolver;
+    Cursor cursor;
 
-        // 画像の情報を取得する
-        ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目(null = 全項目)
-                null, // フィルタ条件(null = フィルタなし)
-                null, // フィルタ用パラメータ
-                null // ソート (null ソートなし)
-        );
 
-        if (cursor.moveToFirst()) {
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            Long id = cursor.getLong(fieldIndex);
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
-        }
-    }*/
 
     private void nextContentsInfo(){
         if (cursor.moveToNext()) {
@@ -187,6 +167,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private void lastContentsInfo(){
+
+        ContentResolver resolver = getContentResolver();
+        Cursor cursor = resolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                null, // 項目(null = 全項目)
+                null, // フィルタ条件(null = フィルタなし)
+                null, // フィルタ用パラメータ
+                null // ソート (null ソートなし)
+        );
+
         if (cursor.moveToLast()) {
             int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
             Long id = cursor.getLong(fieldIndex);
@@ -197,6 +187,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private void firstContentsInfo(){
+
+        this.resolver = getContentResolver();
+        this.cursor = resolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                null, // 項目(null = 全項目)
+                null, // フィルタ条件(null = フィルタなし)
+                null, // フィルタ用パラメータ
+                null // ソート (null ソートなし)
+        );
+
         if (cursor.moveToFirst()) {
             int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
             Long id = cursor.getLong(fieldIndex);
